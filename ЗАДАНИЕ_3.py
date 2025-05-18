bf52b8dcf287baecf5147e71b01d9d6ffd78dbed
ArkhDi <arhipov6355@gmail.com>
2025-05-18 17:27:47 +0300

    Update ЗАДАНИЕ_3.py

--- ЗАДАНИЕ_3.py
+++ ЗАДАНИЕ_3.py
@@ -21,24 +21,24 @@ y0 = f(x10, x20)
 
 fig = plt.figure(figsize=(14, 10))
 
-#изометрический вид
+#IZO
 ax1 = fig.add_subplot(221, projection='3d')
 ax1.plot_surface(X1, X2, Y, cmap='viridis')
 ax1.set_xlabel('x1')
 ax1.set_ylabel('x2')
 ax1.set_zlabel('y=f(x1, x2)')
-ax1.set_title('Трехмерная поверхность (изометрический вид)')
+ax1.set_title('3D (IZO)')
 
-#вид сверху
+#FROM TOP
 ax2 = fig.add_subplot(222, projection='3d')
 ax2.plot_surface(X1, X2, Y, cmap='viridis', zorder=0)
 ax2.view_init(elev=90, azim=-90)
 ax2.set_xlabel('x1')
 ax2.set_ylabel('x2')
 ax2.set_zlabel('y=f(x1, x2)')
-ax2.set_title('Трехмерная поверхность (вид сверху)')
+ax2.set_title('3D(FROM TOP)')
 
-#y = f(x1) при x2 = x20
+#y = f(x1) with x2 = x20
 ax3 = fig.add_subplot(223)
 x1_values = np.linspace(x1_min, x1_max, 500)
 y_values = f(x1_values, x20)
@@ -50,7 +50,7 @@ ax3.set_title(f'y = f(x1) при x2 = {x20}')
 ax3.legend()
 ax3.annotate(f'({x10}, {x20})', (x10, y0), textcoords="offset points", xytext=(0,10), ha='center')
 
-#y = f(x2) при x1 = x10
+#y = f(x2) with x1 = x10
 ax4 = fig.add_subplot(224)
 x2_values = np.linspace(x2_min, x2_max, 500)
 y_values = f(x10, x2_values)
